b990677a506b68501f9dbb0d95fd5de2e1322501
alexrichard1994 <alexandrorichard@hotmail.com>
2020-11-16 17:42:37 -0500

    no muy funciona todavia

---
+++ funcione.js
@@ -0,0 +1,13 @@
+
+function suma(){
+    const primera = document.getElementById("primer").value;
+    const segunda = document.getElementById("segund").value;
+    
+    while(true) {alert (parseInt("primera")+parseInt("segunda"));
+    break
+
+}
+
+}
+const boton = document.getElementById("sumar");
+boton.addEventListener("click",suma);
